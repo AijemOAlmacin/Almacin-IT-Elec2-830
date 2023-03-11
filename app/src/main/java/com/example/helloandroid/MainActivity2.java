a5e47b4d86a4f943c1aa83b39efac6860159fbda
aijemalmacin <aijemalmacin2018@gmail.com>
2023-03-11 12:50:40 +0800

    Creating Fragments

--- app/src/main/java/com/example/helloandroid/MainActivity2.java
+++ app/src/main/java/com/example/helloandroid/MainActivity2.java
@@ -2,25 +2,53 @@ package com.example.helloandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+import android.app.Fragment;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.view.View;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
     Button FragmentA, FragmentB;
-    @SuppressLint("MissingInflatedId")
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        FragmentA = (Button)  findViewById(R.id.FragmentA);
-        FragmentB = (Button) findViewById(R.id.FragmentB);
+    FragmentA = (Button) findViewById(R.id.FragmentA);
+    FragmentB = (Button) findViewById(R.id.FragmentB);
 
-        Fragment
+
+
+        FragmentA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentA());
+            }
+        });
+
+        FragmentB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FragmentB());
+            }
+        });
+
 
 
 
     }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.replace(R.id.constraintLayout, fragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
